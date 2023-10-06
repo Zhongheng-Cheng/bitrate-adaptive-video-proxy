62ee3cfb6ffa163c214cd938d4fa9c7c2e6ac7db
Zhongheng-Cheng <zh_cheng@outlook.com>
2023-10-05 23:44:42 -0400

    added t_*\.py as test

---
+++ t_server.py
@@ -0,0 +1,17 @@
+from socket import *
+
+serverPort = 8080
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print("The server is ready to receive")
+
+while True:
+    connectionSocket, clientAddress = serverSocket.accept()
+    print("Connection established with ", clientAddress)
+    for i in range(10):
+        message = connectionSocket.recv(1024).decode()
+        modifiedMessage = message.upper()
+        connectionSocket.send(modifiedMessage.encode())
+    connectionSocket.close()
+    print("Connection closed")

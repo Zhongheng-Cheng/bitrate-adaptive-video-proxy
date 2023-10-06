2ac761a0ed966a0d2d5d2af473937b33dc851afe
Zhongheng-Cheng <zh_cheng@outlook.com>
2023-10-05 23:13:25 -0400

    added classes Proxy and Client

--- proxy.py
+++ proxy.py
@@ -1,19 +1,43 @@
 import sys
-from socket import *
 
-listen_port, fake_ip, server_ip = sys.argv[1:]
+class Proxy(object):
+    def __init__(self, listen_port, fake_ip, server_ip):
+        import socket as s
 
-proxyPort = int(listen_port)
-proxySocket = socket(AF_INET, SOCK_STREAM)
-proxySocket.bind(('', proxyPort))
-print("The proxy is ready to receive")
-proxySocket.listen(1)
-connectionSocket, clientAddress = proxySocket.accept()
-print("Connection established with ", clientAddress)
+        self.address = ('', int(listen_port))
+        self.fake_ip = fake_ip
+        self.server_ip = server_ip
 
-while True:
-    message = connectionSocket.recv(1024).decode()
-    print("Proxy received from %s: %s" % (clientAddress, message))
-    # connectionSocket.send(message.encode())
-    # connectionSocket.close()
-    # print("Connection closed")
+        self.socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+        self.socket.bind(self.address)
+        return
+
+    def listen_to_connection(self):
+        self.socket.listen(1)
+        print("The proxy is ready to receive")
+        self.current_client_info = self.socket.accept()
+        print("Connection established with ", self.current_client_info)
+        return
+    
+    def receive_message(self, client):
+        message = client.socket.recv(1024).decode()
+        print("Proxy received from %s: %s" % (client.address, message))
+        return
+
+class Client(object):
+    def __init__(self, client_info):
+        self.socket = client_info[0]
+        self.address = client_info[1]
+        return
+    
+if __name__ == '__main__':
+    listen_port, fake_ip, server_ip = sys.argv[1:]
+    proxy = Proxy(listen_port, fake_ip, server_ip)
+    proxy.listen_to_connection()
+    client = Client(proxy.current_client_info)
+    while True:
+        proxy.receive_message(client)
+        
+        # connectionSocket.send(message.encode())
+        # connectionSocket.close()
+        # print("Connection closed")

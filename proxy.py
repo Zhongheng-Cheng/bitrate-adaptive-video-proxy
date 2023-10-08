b632d6bb860d2b81fdcde0554c635bcdf2aa763d
Zhongheng-Cheng <zh_cheng@outlook.com>
2023-10-07 22:49:15 -0400

    edited comments

--- proxy.py
+++ proxy.py
@@ -13,19 +13,21 @@ class Proxy(object):
 
     def connect_to_server(self, server_ip, fake_ip):
         '''
+        Continuously try to connect server at 1 second interval.
+
         Input:
             server_ip: str
             fake_ip: str
         '''
+        print("Connecting to server...")
         while True:
             try:
-                print("Connecting to server...")
                 self.server = InternetEntity((server_ip, 8080))
                 self.server.bind_socket((fake_ip, 0))
                 self.server.conn_socket.connect(self.server.address)
                 break
             except Exception as e:
-                time.sleep(3)
+                time.sleep(1)
         print("Successfully connected to server.")
         return
 
@@ -33,6 +35,7 @@ class Proxy(object):
         '''
         Await a client to connect. Queueing up to 10 clients.
         '''
+        # listen for client connection
         self.socket_listening.listen(10)
         print("The proxy is ready to receive...")
         client_socket, client_address = self.socket_listening.accept()
@@ -44,7 +47,8 @@ class Proxy(object):
     
     def receive_from(self, entity):
         '''
-        Receive a message from an entity socket.
+        Receive a message from an entity's socket.
+        If the message length exceeds the recv buffer length, it keeps receiving until a '\n' appears.
 
         Input: 
             entity: <class 'InternetEntity'>
@@ -62,7 +66,7 @@ class Proxy(object):
     
     def send_to(self, entity):
         '''
-        Send a message to an entity socket.
+        Send a message to an entity's socket.
         
         Input: 
             entity: <class 'InternetEntity'>
@@ -83,7 +87,6 @@ class InternetEntity(object):
             self.conn_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.conn_socket = conn_socket
-
         return
     
     def bind_socket(self, bind_address):
@@ -98,24 +101,28 @@ if __name__ == '__main__':
     # read input
     listen_port, fake_ip, server_ip = sys.argv[1:]
 
-    # server and proxy init
+    # proxy init
     proxy = Proxy(listen_port)
     
-    # client connect
+    # connect client and then server
     proxy.listen_to_connection()
-
-    # server connect
     proxy.connect_to_server(server_ip, fake_ip)
 
     while True:
         try:
+            # forwarding data between server and client
             proxy.receive_from(proxy.client)
             proxy.send_to(proxy.server)
             proxy.receive_from(proxy.server)
             proxy.send_to(proxy.client)
+
         except:
             print("Connection closed")
+
+            # close connection from both sides
             proxy.client.conn_socket.close()
             proxy.server.conn_socket.close()
+
+            # connect client and then server
             proxy.listen_to_connection()
             proxy.connect_to_server(server_ip, fake_ip)

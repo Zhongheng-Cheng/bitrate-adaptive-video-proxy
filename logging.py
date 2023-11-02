06d52457af862b6241cd802e362d71952f3e4327
Zhongheng-Cheng <zc2737@columbia.edu>
2023-11-01 22:06:56 -0400

    renamed logs.py to logging.py

---
+++ logging.py
@@ -0,0 +1,15 @@
+class Logging(object):
+    def __init__(self, filepath):
+        '''
+        Proxy logging: <time> <duration> <tput> <avg-tput> <bitrate> <server-ip> <chunkname>
+        DNS Server logging: 
+            After each response from a client: <time> "request-report" <decision-method> <returned-web-server-ip>
+            After each measurement to the web server: <time> "measurement-report" <video-server-ip> <latency>
+        '''
+        self.filepath = filepath
+        return
+
+    def log(self):
+        with open(self.filepath, 'a') as fo:
+            fo.write("logging...\n")
+        return

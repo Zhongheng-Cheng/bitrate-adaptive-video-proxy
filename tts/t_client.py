770fa7a691bc5770bf8d8eb6a85b187fb4673215
Zhongheng-Cheng <zc2737@columbia.edu>
2023-11-03 17:56:52 -0400

    fixed a bug in t_client.py

--- tts/t_client.py
+++ tts/t_client.py
@@ -1,12 +1,12 @@
 from socket import *
 
 serverName = "127.0.0.1"
-serverPort = 8012
+serverPort = int(input("port: "))
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 print("Successfully connected")
 while True:
-    message = input("Sending message: ")
+    message = input("Sending message: ") + '\n'
     clientSocket.send(message.encode())
     modifiedMessage = clientSocket.recv(1024)
     print("From server: ", modifiedMessage.decode())

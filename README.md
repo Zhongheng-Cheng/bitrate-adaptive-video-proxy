8cb6bc9c1333eeb1588c203ea149b2f07f2b4a73
Zhongheng-Cheng <zc2737@columbia.edu>
2023-11-04 22:21:05 -0400

    implemented DNS reponse for dig

--- README.md
+++ README.md
@@ -13,3 +13,16 @@
 ### Chrome Remote Desktop
 
 <https://remotedesktop.google.com/access/>
+
+## Test inputs
+
+```bash
+# proxy.py: topo_dir, log_path, alpha, listen_port, fake_ip, dns_server_port
+python proxy.py ../topos/topo1 proxy1_log.txt 0.5 8011 127.0.0.1 53
+
+# dns_server.py: topo_dir, log_path, listen_port, decision_method
+python dns_server.py ../topos/topo1 dns_log.txt 53 xx
+
+# dig
+dig @localhost video.colmbia.edu
+```
--- dns_server.py
+++ dns_server.py
@@ -2,10 +2,10 @@
 import socket
 import sys
 
-class DNSServer(object):
+class DnsServer(object):
     def __init__(self):
-        self.DNS_RECORDS = {
-            'video.columbia.edu': 'x.x.x.x', # point to the "best" server's ip
+        self.dns_records = {
+            b'\x05video\x08columbia\x03edu': '1.1.1.1', # point to the "best" server's ip
         }
         topo_dir, log_path, listen_port, decision_method = self.get_inputs()
         self.server_socket = self.listen_to_connection(int(listen_port))
@@ -26,30 +26,55 @@ class DNSServer(object):
 
     def handle_dns_request(self, data, client_address):
         try:
-            # # Parse the DNS request
-            # domain_name = data[12:].split(b'\x00', 1)[0]
-
-            # if domain_name in self.DNS_RECORDS:
-            #     # If the domain name is in our records, send a DNS response with the IP address
-            #     response = (
-            #         data[:2] + b'\x81\x80' + data[4:6] + b'\x00\x00\x00\x00' +
-            #         data[12:] + b'\x00\x01\x00\x01' +
-            #         b'\xc0\x0c\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04' +
-            #         socket.inet_aton(self.DNS_RECORDS[domain_name])
-            #     )
-            #     self.server_socket.sendto(response, client_address)
+            # Parse the DNS request
+            domain_name = data[12:].split(b'\x00', 1)[0]
+            if domain_name in self.dns_records:
+                # If the domain name is in our records, send a DNS response with the IP address
+                response = (
+                    data[:2] +                          # transaction ID
+                    b'\x84\x00' +                       # flags: QR = 1, Opcode, AA = 1, TC, RD, RA, Z, RCODE  
+                    b'\x00\x01' +                       # QDCOUNT = 1
+                    b'\x00\x01' +                       # ANCOUNT = 1
+                    b'\x00\x00' +                       # NSCOUNT = 0
+                    b'\x00\x00' +                       # ARCOUNT = 0
+                    domain_name + b'\x00' +             # domain_name
+                    b'\x00\x01' +                       # QTYPE = 1
+                    b'\x00\x01' +                       # QCLASS = 1
+                    b'\xc0\x0c' +                       # NAME
+                    b'\x00\x01' +                       # TYPE = 1
+                    b'\x00\x01' +                       # CLASS = 1
+                    b'\x00\x00\x00\x00' +               # TTL = 0
+                    b'\x00\x04' +                       # RDLENGTH = 4
+                    self.ip_to_hex(self.dns_records[domain_name])
+                )
+                self.server_socket.sendto(response, client_address)
             return
 
         except Exception as e:
             print(f"Error handling DNS request: {e}")
+            return
+    
+    def ip_to_hex(self, ip: str):
+        result = b''
+        for i in ip.split('.'):
+            result += bytes([int(i)])
+        return result
 
     def serve(self):
         while True:
             data, client_address = self.server_socket.recvfrom(1024)
-            data = data.decode()
             print(f"Received DNS request from {client_address}: {data}")
             self.handle_dns_request(data, client_address)
 
+
+# def resolve_dns(dns_request):
+#     header = dns_request[0:7]
+#     body = dns_request[7:]
+#     return header, body
+
+# def make_dns_request():
+#     pass
+
 if __name__ == '__main__':
-    dns_server = DNSServer()
+    dns_server = DnsServer()
     dns_server.serve()

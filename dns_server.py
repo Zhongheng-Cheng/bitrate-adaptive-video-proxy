8ffa582983ece9355dd946064342fed5b10855b2
Zhongheng-Cheng <zc2737@columbia.edu>
2023-11-05 12:59:12 -0500

    completed logging in dns_server

--- dns_server.py
+++ dns_server.py
@@ -8,7 +8,8 @@ class DnsServer(object):
         self.dns_records = [b'\x05video\x08columbia\x03edu']
         topo_dir, log_path, listen_port, decision_method = self.get_inputs()
         self.server_socket = self.listen_to_connection(int(listen_port))
-        self.topo_dir = topo_dir
+        self.ip_list = self.get_ip_list(topo_dir)
+        self.current_latency = {}
         self.logger = Logger(log_path)
         self.decision_method = decision_method
         return
@@ -22,20 +23,31 @@ class DnsServer(object):
         Listen to UDP connections.
         '''
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        server_socket.settimeout(1)
         server_socket.bind(('0.0.0.0', listen_port))
         print(f"DNS server listening on 0.0.0.0:{listen_port}")
         return server_socket
     
-    def get_ip_list(self) -> list:
-        with open(f"{self.topo_dir}/{self.topo_dir[-5:]}.servers", 'r') as fo:
+    def measure_latency(self):
+        for i in self.ip_list:
+            latency = 100 # TODO: measure the latency of each available server ip
+            self.logger.log(f'measurement-report {i} {latency}')
+            self.current_latency[i] = latency
+        print(self.current_latency)
+        return
+    
+    def get_ip_list(self, topo_dir) -> list:
+        with open(f"{topo_dir}/{topo_dir[-5:]}.servers", 'r') as fo:
             content = fo.readlines()
         ip_list = [i.strip() for i in content]
         return ip_list
     
     def get_best_ip(self) -> str:
-        ip_list = self.get_ip_list()
-        # TODO: Choose the "BEST" server ip
-        best_ip = ip_list[0] ### temp
+        if self.decision_method == "round-robin":
+            pass # TODO: Choose the "BEST" server ip
+        elif self.decision_method == "lowest-latency":
+            pass # TODO: Choose the "BEST" server ip
+        best_ip = self.ip_list[0] ### temp
         return best_ip
 
     def handle_dns_request(self, data, client_address):
@@ -60,7 +72,7 @@ class DnsServer(object):
                 b'\x00\x04' +                       # RDLENGTH = 4
                 self.ip_to_hex(server_ip)
             )
-            self.logger.log(f'"request-report" {self.decision_method} {server_ip}')
+            self.logger.log(f'request-report {self.decision_method} {server_ip}')
         else:
             response = (
                 data[:2] +                          # transaction ID
@@ -84,9 +96,14 @@ class DnsServer(object):
 
     def serve(self):
         while True:
-            data, client_address = self.server_socket.recvfrom(1024)
-            print(f"Received DNS request from {client_address}: {data}")
-            self.handle_dns_request(data, client_address)
+            try:
+                data, client_address = self.server_socket.recvfrom(1024)
+                print(f"Received DNS request from {client_address}: {data}")
+                self.handle_dns_request(data, client_address)
+            except socket.timeout:
+                self.measure_latency()
+            except Exception as e:
+                print(e)
 
 if __name__ == '__main__':
     dns_server = DnsServer()
